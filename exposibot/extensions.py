030456792ecc4c296baf645609b2ff60b28b9998
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-09-08 07:50:41 -0300

    Add migrations and rate limiting extensions

--- exposibot/extensions.py
+++ exposibot/extensions.py
@@ -1,7 +1,14 @@
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
+migrate = Migrate()
+limiter = Limiter(key_func=get_remote_address, default_limits=[])
+
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.login_message = "Faça login para continuar."
